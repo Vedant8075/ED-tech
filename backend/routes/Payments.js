8a1e6a5bb7680fcb0b11946ce1f5920e1bfe45db
VEDANT <vedantg663@gmail.com>
2026-04-21 13:18:44 +0530

    Feat: Add Razorpay and cleanup env.

--- backend/routes/Payments.js
+++ backend/routes/Payments.js
@@ -5,5 +5,5 @@ const { capturePayment, verifyPayment } = require("../controllers/Payment");
 const { auth, isStudent } = require("../middleware/auth");
 router.post("/capturePayment", auth, isStudent, capturePayment);
 router.post("/verifyPayment", auth, isStudent, verifyPayment);
-
+router.post("/sendPaymentSuccessEmail",auth.isStudent,sendPaymentSuccessEmail)
 module.exports = router;
